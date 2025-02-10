a3c6e2e7160ed359df408f11cc6bb6c8659a2e6f
gokmentas <56593104+gokmentas@users.noreply.github.com>
2025-02-10 15:31:25 +0300

    Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,34 @@
+// Purpose: Detect when the player is thrusting using the new Input System.
+// Usage: Attach this script to the player object and assign the thrust action in the inspector.
+// To test this script, create a new Input Action in the Input Actions asset and assign the "Thrust" binding to a key on the keyboard.
+// Press the assigned key to see the "Thrusting" message in the console.
+
+// Import the UnityEngine namespace
 using UnityEngine;
+// Import the InputSystem namespace
 using UnityEngine.InputSystem;
 
+// Create a Movement class
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    // The InputAction object that will be used to detect thrusting
+    [SerializeField] InputAction thrust;
 
+    // onenable is called when the script is enabled
+    private void OnEnable()
+    {
+        // Enable the thrust action
+        thrust.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    // update is called once per frame
+    private void Update()
     {
-
+        // Check if the thrust action is pressed
+        if (thrust.IsPressed())
+        {
+            // Log a message to the console
+            Debug.Log("Thrusting");
+        }
     }
 }
