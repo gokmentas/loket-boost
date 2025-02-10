920bd287100540be3033d86cb8624858a8fcd865
gokmentas <56593104+gokmentas@users.noreply.github.com>
2025-02-10 17:42:37 +0300

    movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,39 +1,51 @@
-// Purpose: Detect when the player is thrusting using the new Input System.
-// Usage: Attach this script to the player object and assign the thrust action in the inspector.
-// To test this script, create a new Input Action in the Input Actions asset and assign the "Thrust" binding to a key on the keyboard.
-// Press the assigned key to see the "Thrusting" message in the console.
+// Purpose: To move the player object in the up direction and rotate it to the left or right using the thrust and rotation actions.
+// Usage: Attach this script to the player object and assign the thrust and rotation bindings in the inspector.
+// To use the thrust action, press the assigned key. To use the rotation action, press the assigned keys to rotate the player object to the left or right.
+// The player object will move in the up direction and rotate to the left or right based on the input provided by the thrust and rotation actions.
+// The strength of the thrust and rotation can be adjusted in the inspector.
 
-// Import the UnityEngine namespace
+// Import the necessary namespaces
 using UnityEngine;
-// Import the InputSystem namespace
 using UnityEngine.InputSystem;
 
 // Create a Movement class
 public class Movement : MonoBehaviour
 {
-    // The InputAction object that will be used to detect thrusting
+    // Store the thrust and rotation actions
     [SerializeField] InputAction thrust;
-    // The strength of the thrust
+    [SerializeField] InputAction rotation;
+
+    // The strength of the thrust and rotation
     [SerializeField] float thrustStrength = 1000f;
+    [SerializeField] float rotationStrength = 100f;
+
     // Store the Rigidbody component of the player object
     Rigidbody rb;
 
-    // Start is called before the first frame update
+    // Doing this on start because we need to get the Rigidbody component
     private void Start()
     {
         // Get the Rigidbody component of the player object
         rb = GetComponent<Rigidbody>();
     }
 
-    // onenable is called when the script is enabled
+    // Doing this on enable imstead of start because we need to enable the actions
     private void OnEnable()
     {
-        // Enable the thrust action
+        // Enable the thrust and rotation actions
         thrust.Enable();
+        rotation.Enable();
     }
 
-    // fixedupdate is different from update as it is called at a fixed interval
+    // Calling movement methods in FixedUpdate instead of Update because we are using physics
     private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    // ProcessThrust is a method that applies a force to the player object in the up direction
+    private void ProcessThrust()
     {
         // Check if the thrust action is pressed
         if (thrust.IsPressed())
@@ -41,5 +53,31 @@ public class Movement : MonoBehaviour
             // Apply a force to the player object in the up direction
             rb.AddRelativeForce(thrustStrength * Time.fixedDeltaTime * Vector3.up);
         }
+    }
+
+    // ProcessRotation is a method that rotates the player object
+    private void ProcessRotation()
+    {
+        // Get the value of the rotation action
+        float rotationValue = rotation.ReadValue<float>();
+
+        // Check if the rotation value is less than 0 or greater than 0 and apply rotation accordingly
+        if (rotationValue < 0)
+        {
+            // Rotate the player object to the left
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationValue > 0)
+        {
+            // Rotate the player object to the right
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    // ApplyRotation is a method that rotates the player object
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        // Rotate the player object
+        transform.Rotate(rotationThisFrame * Time.fixedDeltaTime * Vector3.forward);
     }
 }
